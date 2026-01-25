2a06949a0db9ccc931df6fa019a2d3009d734054
Paulo Santos <paulo.f.santos@glintt.com>
2026-01-25 08:16:09 +0000

    search endpoint completed without tests

--- src/features/search/search-endpoint.ts
+++ src/features/search/search-endpoint.ts
@@ -1,6 +1,14 @@
 import { HttpClient } from '../../http-client.interface';
 import { PaginatedResult } from '../../shared';
-import { SearchCollectionResult } from './search.types';
+import {
+    SearchCollectionResult,
+    SearchCompaniesResult,
+    SearchKeywordsResult,
+    SearchMoviesResult,
+    SearchMultiResult,
+    SearchPeopleResult,
+    SearchTvShowsResult,
+} from './search.types';
 
 export class SearchEndpoint {
     public constructor(private readonly client: HttpClient) {}
@@ -8,16 +16,100 @@ export class SearchEndpoint {
     /**
      * Search for collections by their original, translated and alternative names
      */
-    public async collection(options: {
-        query: string;
-        includeAdult?: boolean;
-        language?: string;
-        page?: number;
-        region?: string;
-    }): Promise<PaginatedResult<SearchCollectionResult>> {
-        return await this.client.get<PaginatedResult<SearchCollectionResult>>(
-            '/search/collection',
-            options
-        );
+    public async collections(
+        options: Readonly<{
+            query: string;
+            includeAdult?: boolean;
+            language?: string;
+            page?: number;
+            region?: string;
+        }>
+    ): Promise<PaginatedResult<SearchCollectionResult>> {
+        return await this.client.get('/search/collection', options);
+    }
+
+    /**
+     * Search for companies by their original and alternative names
+     */
+    public async companies(
+        options: Readonly<{
+            query: string;
+            page?: number;
+        }>
+    ): Promise<PaginatedResult<SearchCompaniesResult>> {
+        return await this.client.get('/search/company', options);
+    }
+
+    /**
+     * Search for keywords by their name
+     */
+    public async keywords(
+        options: Readonly<{
+            query: string;
+            page?: number;
+        }>
+    ): Promise<PaginatedResult<SearchKeywordsResult>> {
+        return await this.client.get('/search/keyword', options);
+    }
+
+    /**
+     * Search for movies by their original, translated and alternative titles
+     */
+    public async movies(
+        options: Readonly<{
+            query: string;
+            includeAdult?: boolean;
+            language?: string;
+            primaryReleaseYear?: string;
+            page?: number;
+            region?: string;
+            year?: string;
+        }>
+    ): Promise<PaginatedResult<SearchMoviesResult>> {
+        return await this.client.get('/search/movie', options);
+    }
+
+    /**
+     * Use multi search when you want to search for movies, TV shows and people in a single request
+     */
+    public async multi(
+        options: Readonly<{
+            query: string;
+            includeAdult?: boolean;
+            language?: string;
+            page?: number;
+        }>
+    ): Promise<PaginatedResult<SearchMultiResult>> {
+        return await this.client.get('/search/multi', options);
+    }
+
+    /**
+     * Search for people by their name and also known as names
+     */
+    public async people(
+        options: Readonly<{
+            query: string;
+            includeAdult?: boolean;
+            language?: string;
+            page?: number;
+        }>
+    ): Promise<PaginatedResult<SearchPeopleResult>> {
+        return await this.client.get('/search/person', options);
+    }
+
+    /**
+     * Search for TV shows by their original, translated and also known as names
+     */
+    public async tvShows(
+        options: Readonly<{
+            query: string;
+            firstAirDateYear?: number;
+            includeAdult?: boolean;
+            language?: string;
+            page?: number;
+            year?: number;
+        }>
+    ): Promise<PaginatedResult<SearchTvShowsResult>> {
+        return await this.client.get('/search/tv', options);
     }
 }
--- src/features/search/search.types.ts
+++ src/features/search/search.types.ts
@@ -1,3 +1,5 @@
+import { GenderType, MediaType } from '../../shared';
+
 export type SearchCollectionResult = Readonly<{
     adult: boolean;
     backdropPath: string;
@@ -7,4 +9,91 @@ export type SearchCollectionResult = Readonly<{
     originalName: string;
     overview: string;
     posterPath: string;
+}>;
+export type SearchCompaniesResult = Readonly<{
+    id: number;
+    logoPath: string;
+    name: string;
+    originCountry: string;
+}>;
+export type SearchKeywordsResult = Readonly<{
+    id: number;
+    name: string;
+}>;
+export type SearchMoviesResult = Readonly<{
+    adult: boolean;
+    backdropPath: string;
+    genreIds: Readonly<number[]>;
+    id: number;
+    originalLanguage: string;
+    originalTitle: string;
+    overview: string;
+    popularity: number;
+    posterPath: string;
+    releaseDate: string;
+    title: string;
+    video: boolean;
+    voteAverage: number;
+    voteCount: number;
+}>;
+export type SearchMultiResult = Readonly<{
+    adult: boolean;
+    backdropPath: string;
+    id: number;
+    title: string;
+    originalLanguage: string;
+    originalTitle: string;
+    overview: string;
+    posterPath: string;
+    mediaType: MediaType;
+    genreIds: Readonly<number[]>;
+    popularity: number;
+    releaseDate: string;
+    video: boolean;
+    voteAverage: number;
+    voteCount: number;
+}>;
+export type SearchPeopleKnownForItem = Readonly<{
+    adult: boolean;
+    backdropPath: string;
+    id: number;
+    title: string;
+    originalLanguage: string;
+    originalTitle: string;
+    overview: string;
+    posterPath: string;
+    mediaType: MediaType;
+    genreIds: Readonly<number[]>;
+    popularity: number;
+    releaseDate: string;
+    video: boolean;
+    voteAverage: number;
+    voteCount: number;
+}>;
+export type SearchPeopleResult = Readonly<{
+    adult: boolean;
+    gender: GenderType;
+    id: number;
+    knownForDepartment: Readonly<string[]>;
+    name: string;
+    originalName: string;
+    popularity: number;
+    profilePath: string;
+    knownFor: Readonly<SearchPeopleKnownForItem[]>;
+}>;
+export type SearchTvShowsResult = Readonly<{
+    adult: boolean;
+    backdropPath: string;
+    genreIds: Readonly<number[]>;
+    id: number;
+    originCountry: Readonly<string[]>;
+    originalLanguage: string;
+    originalName: string;
+    overview: string;
+    popularity: number;
+    posterPath: string;
+    firstAirDate: string;
+    name: string;
+    voteAverage: number;
+    voteCount: number;
 }>;
